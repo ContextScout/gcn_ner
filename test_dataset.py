afc2f84df13b01c8219f0bc5f2e18e5cdcfce839
alberto cetoli <alberto.cetoli@nlulite.com>
2017-09-08 09:14:14 +0100

    changed the default saved model

--- test_dataset.py
+++ test_dataset.py
@@ -1,5 +1,5 @@
 from gcn_ner import GCNNer
 
 if __name__ == '__main__':
-    ner = GCNNer(ner_filename='./data/ner-gcn-10.tf', trans_prob_file='./data/trans_prob.pickle')
+    ner = GCNNer(ner_filename='./data/ner-gcn-13.tf', trans_prob_file='./data/trans_prob.pickle')
     ner.test('./data/dev.conll')
